6b6323b02232eab7dbb7f87fee0133a614cca2b9
Boscoochen <61206389+Boscoochen@users.noreply.github.com>
2021-09-07 13:39:32 -0500

    done data loading

--- homework2/homework/utils.py
+++ homework2/homework/utils.py
@@ -2,6 +2,8 @@ from PIL import Image
 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import csv
+import os
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
 
@@ -14,12 +16,30 @@ class SuperTuxDataset(Dataset):
         Your code here
         Hint: Use your solution (or the master solution) to HW1
         """
-        raise NotImplementedError('SuperTuxDataset.__init__')
+        self.dataset_path = dataset_path
+        
+        #dataset_path = "data/train/" 
+        self.img_path = []
+        self.main_path = []
+        self.img_label = []
+        
+        label_csv_path = os.path.join(dataset_path, "labels.csv")
+        with open(label_csv_path) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)
+            for line in csv_reader:
+                self.img_path.append(line[0])
+                
+                self.img_label.append(LABEL_NAMES.index(line[1]))
+        for path in self.img_path:
+            self.main_path.append(os.path.join(self.dataset_path, path))
+        # raise NotImplementedError('SuperTuxDataset.__init__')
 
     def __len__(self):
         """
         Your code here
         """
+        return len(self.img_path)
         raise NotImplementedError('SuperTuxDataset.__len__')
 
     def __getitem__(self, idx):
@@ -27,6 +47,15 @@ class SuperTuxDataset(Dataset):
         Your code here
         return a tuple: img, label
         """
+        img_item_path = self.main_path[idx]
+        img = Image.open(img_item_path)
+        #transforms.Compose([transforms.Resize(3,64,64), transforms.ToTensor(),transforms.Normalize([0],[1])])
+        trans_totensor = transforms.ToTensor()
+        img_tensor = trans_totensor(img)
+        img_tensor = img_tensor.reshape(3, 64, 64)
+        
+        label = self.img_label[idx]
+        return img_tensor, label
         raise NotImplementedError('SuperTuxDataset.__getitem__')
 
 
