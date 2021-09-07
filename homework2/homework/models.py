ca190596ed343ec53ff21e621cc484a8068d2230
Boscoochen <61206389+Boscoochen@users.noreply.github.com>
2021-09-07 13:38:47 -0500

    done model

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -1,13 +1,22 @@
 import torch
 from torch import nn
 from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import BatchNorm2d
 class CNNClassifier(torch.nn.Module):
     def __init__(self):
         """
         Your code here
         """
         super(CNNClassifier, self).__init__()
+        self.conv1_bn = BatchNorm2d(3)
         self.conv1 = Conv2d(3,6,3,stride=1,padding=0)
+        self.relu1 = ReLU()
+        self.conv2_bn = BatchNorm2d(6)
+        self.linear1 = Linear(6*62*62, 100)
+        self.relu2 = ReLU()
+        self.linear2 = Linear(100, 6)
         # raise NotImplementedError('CNNClassifier.__init__')
 
     def forward(self, x):
@@ -16,8 +25,16 @@ class CNNClassifier(torch.nn.Module):
         @x: torch.Tensor((B,3,64,64))
         @return: torch.Tensor((B,6))
         """
+        # print(x.shape)
+        x = self.conv1_bn(x)
         x = self.conv1(x)
-        return x
+        x = self.relu1(x)
+        x = self.conv2_bn(x)
+        x = self.linear1((x.view(x.size(0), -1)))
+        x = self.relu2(x)
+        return self.linear2((x.view(x.size(0), -1)))
+        # print(x.shape)
+        # return x
         raise NotImplementedError('CNNClassifier.forward')
 
 
