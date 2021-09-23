8ce9716b3a9a96d803a7eae2ed0d2b9ecd2e0971
Boscoochen <61206389+Boscoochen@users.noreply.github.com>
2021-09-22 22:55:12 -0500

    done hw3

--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -5,10 +5,13 @@ from .models import FCN, save_model
 from .utils import load_dense_data, DENSE_CLASS_DISTRIBUTION, ConfusionMatrix
 from . import dense_transforms
 import torch.utils.tensorboard as tb
+from torch import nn
 
 
 def train(args):
     from os import path
+    device1 = torch.device("cuda")
+    device2 = torch.device("cpu")
     model = FCN()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
@@ -22,7 +25,78 @@ def train(args):
     Hint: If you found a good data augmentation parameters for the CNN, use them here too. Use dense_transforms
     Hint: Use the log function below to debug and visualize your model
     """
+    print("code here")
+        
+    traindata_path = "dense_data/train"
+    testdata_path = "dense_data/valid"
+
+    loss_fun = nn.CrossEntropyLoss().to(device1)
+    train_dataloader = load_dense_data(traindata_path, num_workers=4, batch_size=32)
+    test_dataloader = load_dense_data(testdata_path, num_workers=4, batch_size=32)
+
+    # print(train_dataloader)
+    # print(test_dataloader)
+
+
+    # print(model)
+    learning_rate = 0.005
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    total_train_step = 0
+    total_test_step = 0
+    epoch = 15
+
+    for i in range(epoch):
+      model.train()
+      for data in train_dataloader:
+        imgs, targets = data
+        # print(type(imgs))
+        # print(len(targets))
+        # print(imgs.shape)
+        #torch.Size([128, 3, 64, 64])
+        outputs = model(imgs).to(device1)
+        #torch.Size([128, 6, 62, 62])
+        # print(outputs.shape)
+        # outputs = outputs.view(outputs.size(0), -1)
+        # print(outputs.shape)
+        targets =targets.long().to(device2)
+        
+        
+        targets = targets.to(device1)
+        loss = loss_fun(outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total_train_step = total_train_step + 1
+        # print(total_train_step)
+        if total_train_step % 100 == 0:
+          print("times train: {}, Loss: {}".format(total_train_step,loss))
+    
+    model.eval()
+    total_test_loss = 0
+    acc = []
+    confusion = ConfusionMatrix(6)
+    with torch.no_grad():
+      for data in test_dataloader:
+        imgs, targets = data
+        outputs = model(imgs).to(device1)
+        # outputs = outputs.view(outputs.size(0), -1)
+        targets = targets.long().to(device2)
+        targets = targets.to(device1)
+        loss = loss_fun(outputs, targets)
+        total_test_loss = total_test_loss + loss
+        confusion.add(outputs.argmax(1), targets)
+        outputs_idx = outputs.max(1)[1].type_as(targets)
+        acc.append(outputs_idx.eq(targets).float().mean())
+        
+    print("total test loss: {}".format(total_test_loss))
+    print(confusion.global_accuracy)
+    
+    #raise NotImplementedError('train')
     save_model(model)
+    print(sum(acc)/len(acc))
+    return sum(acc)/len(acc)
 
 
 def log(logger, imgs, lbls, logits, global_step):
@@ -47,6 +121,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
-
+    parser.add_argument('-m', '--model', default='fcn')
     args = parser.parse_args()
     train(args)
